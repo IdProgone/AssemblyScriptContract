20ab66992cd48702d7729ce9160b00f955c68620
IdProgone <mahdi-youssef@hotmail.fr>
2022-10-31 15:53:13 +0000

    correct AcountID no Index.ts

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,7 +2,7 @@ import { storage, Context } from "near-sdk-as";
 
 export class Greeting {
 
-getGreeting(accoundId: string): string | null {
+getGreeting(accountId: string): string | null {
 
    return storage.get<string>(accountId, 
     "Greeting Not available");
